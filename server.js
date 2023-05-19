0e425008809c1430bc43cbc3813fa5a91cbd8a57
katvengo <katvengo@gmail.com>
2023-05-19 10:50:46 -0700

    changed server.js

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
-var express = require('express');
-
-var PORT = process.env.PORT || 8080;
+var express = require("express");
 
 var app = express();
 
-var db = require('./models');
+var db = require("./models");
 
 
-app.use(express.static('public'));
-
-app.use(express.urlencoded({
-    extended: true
-}));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("public"));
 require("./routes/html-routes.js")(app);
 
-    app.listen(PORT, function () {
-        console.log(`'Server listening at http://localhost:${PORT}`)
-    })
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('./public', 'index.html'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, './public/', 'index.html'));
+    });
+  } 
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
